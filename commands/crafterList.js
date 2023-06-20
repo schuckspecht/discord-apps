2c7609b798e748ea321625a2b9a442f4794c3d7e
Philipp Schuck <philipp.schuck@bauerberatung.de>
2023-06-20 17:34:17 +0200

    Updated package

--- commands/crafterList.js
+++ commands/crafterList.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const { EmbedBuilder } = require("discord.js");
 require("dotenv").config();
 
--- commands/recipeList.js
+++ commands/recipeList.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const { ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
 // Connection configuration
 const dbConfig = {
