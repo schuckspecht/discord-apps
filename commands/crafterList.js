ba86638ab1d06bc9985a0f5b3c051ac9489effd0
Philipp Schuck <philipp.schuck@bauerberatung.de>
2023-06-20 17:38:31 +0200

    Revert

--- commands/crafterList.js
+++ commands/crafterList.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 // Connection configuration
 const dbConfig = {
   host: process.env.DB_HOST,
-  user: "root",
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 };
